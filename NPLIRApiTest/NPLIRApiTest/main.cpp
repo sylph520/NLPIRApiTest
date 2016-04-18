9511d32b9784714620060fd41327b5b8d7f80fb9
sylph520 <pallasjack@outlook.com>
2016-04-18 09:23:11 +0800

    Initial config

---
+++ NPLIRApiTest/NPLIRApiTest/main.cpp
@@ -0,0 +1,12 @@
+#include "NLPIR.h"
+#pragma comment(lib, "NLPIR.lib")
+
+#include <iostream>
+
+using namespace std;
+
+const string DataPath = "";
+int main(int argc, char *argv[])
+{
+	return 0;
+}
